4cf39bd2f1b488ca749f15f22b7f417a5cb6d4cb
Jaden Sierra <JadenSierra31@gmail.com>
2022-03-11 12:40:23 -0800

    Initial_commit

---
+++ src/Components/Form.jsx
@@ -0,0 +1,95 @@
+import React, {useState} from "react";
+
+const Form = props => {
+
+    const [fName, setFName] = useState("");
+    const [lName, setLName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const [fNameError, setFNameError] = useState("");
+    const [lNameError, setLNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const handleFName = (e) => {
+        setFName(e.target.value);
+        if(fName.length < 1){
+            setFNameError("Please type a Name")
+        } else if (fName.length < 2){
+            setFNameError("Name must be longer than 2 characters")
+        }
+    }
+
+    const handleLName = (e) => {
+        setLName(e.target.value);
+        if(lName.length < 1){
+            setLNameError("Please type a Name")
+        } else if (lName.length < 2){
+            setLNameError("Name must be longer than 2 characters")
+        }
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(email.length < 1){
+            setEmailError("Please type an email")
+        } else if (email.length < 2){
+            setEmailError("Email must be longer than 5 characters")
+        }
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if(password.length < 1){
+            setPasswordError("Please type a password")
+        } else if (password.length < 8){
+            setPasswordError("Password must be longer than 8 characters")
+        }
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+        if(confirmPassword.length < 1){
+            setConfirmPasswordError("Please confirm password")
+        } else if (password !== confirmPassword){
+            setConfirmPasswordError("Passwords do not match")
+        }
+    }
+
+    return (
+        <>
+        <form>
+            <div>
+                <label>First Name</label>
+                <input type="text" onChange={handleFName}/>
+                {fNameError ? <p style={{color:"red"}}>{fNameError}</p> : ""}
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input type="text" onChange={handleLName}/>
+                {lNameError ? <p style={{color:"red"}}>{lNameError}</p> : ""}
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="text" onChange={handleEmail}/>
+                {emailError ? <p style={{color:"red"}}>{emailError}</p> : ""}
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="text" onChange={handlePassword}/>
+                {passwordError ? <p style={{color:"red"}}>{passwordError}</p> : ""}
+            </div>
+            <div>
+                <label>Confirm Password</label>
+                <input type="text" onChange={handleConfirmPassword}/>
+                {confirmPasswordError ? <p style={{color:"red"}}>{confirmPasswordError}</p> : ""}
+            </div>
+        </form>
+        </>
+    );
+}
+
+export default Form;
